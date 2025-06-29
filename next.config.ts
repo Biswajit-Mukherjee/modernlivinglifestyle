78e0f801f5d17229dd75a112a508f05cdaa41268
Biswajit <biswajitmukherjee549@gmail.com>
2025-06-29 17:45:19 +0530

    config: update next.config.js

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const ContentSecurityPolicy = `
   img-src 'self' data: blob: https://cdn.sanity.io https://images.unsplash.com https://www.google-analytics.com https://pagead2.googlesyndication.com;
   font-src 'self' https://fonts.gstatic.com;
   connect-src 'self' https://cdn.sanity.io https://*.sanity.io https://va.vercel-scripts.com https://www.google-analytics.com https://region1.google-analytics.com https://ep1.adtrafficquality.google;
-  frame-src https://www.youtube.com https://player.vimeo.com https://googleads.g.doubleclick.net;
+  frame-src https://www.youtube.com https://player.vimeo.com https://googleads.g.doubleclick.net https://ep2.adtrafficquality.google https://www.google.com;
   object-src 'none';
   base-uri 'self';
   form-action 'self';
