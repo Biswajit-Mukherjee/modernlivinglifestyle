413951a602550b7b6155c6df4fe06b3154569f43
Biswajit <biswajitmukherjee549@gmail.com>
2025-06-29 17:57:01 +0530

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -8,10 +8,11 @@ const ContentSecurityPolicy = `
   default-src 'self';
   script-src 'self' 'unsafe-inline' ${IS_DEV ? "'unsafe-eval' http://localhost:3000/api/auth/* https://cdn.jsdelivr.net/npm/@scalar/api-reference https://va.vercel-scripts.com/v1/script.debug.js https://va.vercel-scripts.com/v1/speed-insights/script.debug.js" : ""} https://va.vercel-scripts.com/v1/speed-insights/script.js https://cdn.sanity.io https://www.googletagmanager.com https://www.google-analytics.com https://pagead2.googlesyndication.com https://ep2.adtrafficquality.google;
   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-  img-src 'self' data: blob: https://cdn.sanity.io https://images.unsplash.com https://www.google-analytics.com https://pagead2.googlesyndication.com;
+  img-src 'self' data: blob: https://cdn.sanity.io https://images.unsplash.com https://www.google-analytics.com https://pagead2.googlesyndication.com https://ep1.adtrafficquality.google;
   font-src 'self' https://fonts.gstatic.com;
   connect-src 'self' https://cdn.sanity.io https://*.sanity.io https://va.vercel-scripts.com https://www.google-analytics.com https://region1.google-analytics.com https://ep1.adtrafficquality.google;
   frame-src https://www.youtube.com https://player.vimeo.com https://googleads.g.doubleclick.net https://ep2.adtrafficquality.google https://www.google.com;
+  fenced-frame-src '';
   object-src 'none';
   base-uri 'self';
   form-action 'self';
